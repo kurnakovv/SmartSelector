70fa6d73d89d989cbd1c04e1029e5bee53da5f7b
KurnakovMaksim <maksk210@gmail.com>
2023-10-02 18:44:44 +0300

    #16 Add more assert's for test

--- tests/SmartSelector.UnitTests/SelectorExtensionsTests.cs
+++ tests/SmartSelector.UnitTests/SelectorExtensionsTests.cs
@@ -40,6 +40,9 @@ namespace SmartSelector.UnitTests
             });
             string expectedFinalString = "FirstProperty: '', SecondProperty: 'a2', ThirdProperty: 'a3'\r\nFirstProperty: '', SecondProperty: 'b2', ThirdProperty: 'b3'\r\nFirstProperty: '', SecondProperty: 'c2', ThirdProperty: 'c3'";
             Assert.Equal(expectedFinalString, string.Join("\r\n", result.ToList().Select(x => new string($"FirstProperty: '{x.FirstProperty}', SecondProperty: '{x.SecondProperty}', ThirdProperty: '{x.ThirdProperty}'"))));
+            Assert.True(result.Select(x => x.FirstProperty).All(x => x == null));
+            Assert.True(result.Select(x => x.SecondProperty).All(x => x != null));
+            Assert.True(result.Select(x => x.ThirdProperty).All(x => x != null));
         }
     }
 }
